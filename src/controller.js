6e281772d48ea53250b7acaf0a386f0af06f2cec
Sahil Bhalla <bhallas@purdue.edu>
2016-10-28 16:33:38 -0400

    Initial repo.

---
+++ src/controller.js
@@ -0,0 +1,34 @@
+import {Controller} from 'cerebral'
+import model from './model'
+import Devtools from 'cerebral-module-devtools'
+
+import updateItemTitle from './chains/updateItemTitle'
+import addNewItem from './chains/addNewItem'
+
+const controller = Controller(model)
+
+controller.addSignals({
+  newItemTitleChanged: {
+    chain: updateItemTitle,
+    immediate: true
+  },
+  newItemTitleSubmitted: addNewItem,
+
+  aaronControllerOnNewShapeRequested: [
+    function({input, state, output, services}) {
+      var cur_shape = state.get('inStateShape');
+      if (cur_shape === 'circle') {
+        state.set('inStateShape', 'square');
+      } else {
+        state.set('inStateShape', 'circle');
+      }
+    }
+  ],
+
+})
+
+controller.addModules({
+  devtools: Devtools()
+})
+
+export default controller
---
+++ src/model.js
@@ -0,0 +1,9 @@
+import Model from 'cerebral/models/immutable'
+
+const model = Model({
+  items: [],
+  newItemTitle: '',
+  inStateShape: 'circle',
+})
+
+export default model
