deafff212df15b45b2d27ba8c9823c5c0275ca61
Ao Liu <liu837@Purdue.edu>
2016-11-11 12:59:09 -0500

    update the project,work with the push button

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,15 +1,28 @@
+/** @jsx React.DOM */
 import React from 'react'
 import {connect} from 'cerebral-view-react'
 import style from'./function.css'
 export default connect(
-    {},
-    {},
-    function(){
-      return (
-        <div className="outer">
-          <button type="button">ADD</button>
-        
-        </div>
-      );
-    }
-)
+{},
+{},
+
+var tsc = React.createClass({
+  Tscreate :function(){
+    <div className="block">
+      </div>
+  },
+  render: function(){
+    return (
+      <div className="outer">
+        <div className="curve">
+       
+      <button type="button" onClick = {this.Tscreate}>Add</button>
+      
+      </div>
+      </div>
+    );
+  }
+});
+
+React.renderComponent(<tsc/>, document.body);
+);
