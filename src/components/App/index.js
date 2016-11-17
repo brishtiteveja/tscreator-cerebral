33da84b50ebe98634803c0ec728334b8fdef7d11
Sahil Bhalla <bhallas@purdue.edu>
2016-11-16 21:20:00 -0500

    This is working code that Sriram and I did. it adds 4 columns

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,21 +2,30 @@ import React from 'react'
 import {connect} from 'cerebral-view-react'
 import style from'./function.css'
 export default connect(
-	{},
-	{},
-	function App() {
+		{},
+		{},
+		function App() {
 		const tsc = () => {	
-		  return(
-      			<div className="outer">
-				
-        			<div className="curve">
-      					<button type="button" >Add</button>
-      				</div>
-      			</div>
-		  )
+		return(
+			<div className="outer">
+				<div className="curve">
+					<button type="button" >Curve</button>
+				</div>
+				<div className = "block">
+					<button type="button" >Block</button>
+				</div>
+				<div className = "tree">
+					<button type="button" >Tree</button>
+				</div>
+				<div className = "text">
+					<button type="button" >Text</button>
+				</div>
+			</div>
+		      )
 		}
-    		return (
-		    tsc()	
-    		)
-  	}
- );
+
+		return (
+				tsc()	
+		       )
+		}
+);
---
+++ src/components/App/newcode.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {connect} from 'cerebral-view-react'
+import body from'./funtion.css'
+export default body(
+		function(){
+		return (
+				<div outercolonm="outer">
+				<button type="button">ADD</button>
+
+				</div>
+		       );
+		}
+		)
