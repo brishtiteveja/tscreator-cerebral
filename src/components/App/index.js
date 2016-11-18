24fd2d97d0311f75abf3af0ae314772060d9174a
Sriram Baireddy <sbairedd@purdue.edu>
2016-11-18 02:43:32 -0500

    Added a remove button that removes the most recent column added. Next goal would be to implement multiple options for the column to be added.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,16 +5,19 @@ export default connect(
 		{},
 		{},
 		function App() {
-			var num = 0;
 			const addColumn = event => {
 				var container = document.getElementById("container");
-				container.innerHTML += '<div class="column"></div>';
-				console.log(container.innerHTML);
+				container.insertAdjacentHTML('beforeend', '<div class="column"></div>');
+			}
+			const remColumn = event => {
+				var container = document.getElementById("container");
+				container.removeChild(container.lastChild);
 			}
 			const tsc = () => {	
 				return(
 					<div className="outer">
 						<button type="button" onClick={addColumn}>Add Column</button>
+						<button type = "button" onClick={remColumn}>Remove Column</button>
 						<div id="container" className="inner">
 						</div>	
 		      			</div>
