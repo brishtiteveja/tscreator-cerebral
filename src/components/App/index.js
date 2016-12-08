004a1c940498f3196c0c03cdcf2d9cd52430712a
Sriram Baireddy <sbairedd@purdue.edu>
2016-12-08 01:10:35 -0500

    This is the work I did on Wednesday, which primarily consisted of trying to find ways around the signal problem mentioned with controller.js. I got a window to open and contain column buttons, but was unable to edit the divs in the main window when the corresponding button is pressed. Basically, I have a clear idea of what I need to do, but for some reason, I can't get the signals required to work.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,29 +2,34 @@ import React from 'react'
 import {connect} from 'cerebral-view-react'
 import style from'./function.css'
 export default connect(
-		{},
-		{},
-		function App() {
-			const addColumn = event => {
-				var container = document.getElementById("container");
-				container.insertAdjacentHTML('beforeend', '<div class="column"></div>');
-			}
-			const remColumn = event => {
-				var container = document.getElementById("container");
-				container.removeChild(container.lastChild);
-			}
-			const tsc = () => {	
-				return(
-					<div className="outer">
-						<button type="button" onClick={addColumn}>Add Column</button>
-						<button type = "button" onClick={remColumn}>Remove Column</button>
-						<div id="container" className="inner">
-						</div>	
-		      			</div>
-				)
-			}
-			return (
-				tsc()	
-		       	)
+	{},	
+	{
+		addColumn: 'sriramNewColumnRequested',
+	},
+
+	function App() {
+
+		const addColumnButton = () => {
+			var container = document.getElementById("container");
+			container.insertAdjacentHTML('beforeend','<div class="column"></div>');
+			/*var chooseColumn = window.open("","colChooser","height=200 width=400 left=450 top=300");
+			chooseColumn.document.write("<TITLE>Column Chooser</TITLE>");
+			chooseColumn.document.write('<button type="button" onClick={addCurveColumn}>Curve</button>');
+			chooseColumn.document.write('<button type="button" onClick={addTreeColumn}>Tree</button>');
+			chooseColumn.document.write('<button type="button" onClick={addBlockColumn}>Block</button>');
+			chooseColumn.document.write('<button type="button" onClick={addTransectColumn}>Transect</button>');*/
 		}
+		const remColumn = event => {
+			var container = document.getElementById("container");
+			container.removeChild(container.lastChild);
+		}
+		return(
+			<div className="outer">
+				<button type = "button" onClick={addColumn}>Add Column</button>
+				<button type = "button" onClick={remColumn}>Remove Column</button>
+				<div id="container" className="inner">
+				</div>	
+	      		</div>
+		)
+	}
 );
