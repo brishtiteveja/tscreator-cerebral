4fb118ab727474e72ea54b0218249c48c54a4b1c
Sriram Baireddy <sbairedd@purdue.edu>
2016-12-08 01:08:18 -0500

    Added a new signal, but for some reason it's not recognized locally in index.js. This is what I did on Monday: better understood how signals, actions and chains worked, and attempted to add my own

--- src/controller.js
+++ src/controller.js
@@ -8,27 +8,33 @@ import addNewItem from './chains/addNewItem'
 const controller = Controller(model)
 
 controller.addSignals({
-  newItemTitleChanged: {
-    chain: updateItemTitle,
-    immediate: true
-  },
-  newItemTitleSubmitted: addNewItem,
+	newItemTitleChanged: {
+		chain: updateItemTitle,
+		immediate: true
+	},
 
-  aaronControllerOnNewShapeRequested: [
-    function({input, state, output, services}) {
-      var cur_shape = state.get('inStateShape');
-      if (cur_shape === 'circle') {
-        state.set('inStateShape', 'square');
-      } else {
-        state.set('inStateShape', 'circle');
-      }
-    }
-  ],
+	newItemTitleSubmitted: addNewItem,
+	
+	aaronControllerOnNewShapeRequested: [
+		function({input, state, output, services}) {
+			var cur_shape = state.get('inStateShape');
+			if (cur_shape === 'circle') {
+				state.set('inStateShape', 'square');
+			} else {
+				state.set('inStateShape', 'circle');
+			}
+		}
+	],
 
+	sriramNewColumnRequested: [
+		function({input, state, output, services}) {
+			window.open("https://google.com");
+		}
+	],
 })
 
 controller.addModules({
-  devtools: Devtools()
+	devtools: Devtools()
 })
 
 export default controller
