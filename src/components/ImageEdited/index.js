725dbcb8df7e9af106011f56b8076b137435e33e
Sriram Baireddy <sbairedd@purdue.edu>
2017-02-24 15:05:10 -0500

    The image is now displayed accurately in the svg. The next step is to be able tp track where the line is clicked relative to the image location.

--- src/components/ImageEdited/index.js
+++ src/components/ImageEdited/index.js
@@ -10,9 +10,19 @@ export default connect(
 	},
 
 	function ImageEdited(props) {
+		var myImage = new Image();
+		myImage.src = props.imags[props.chosen];
+		var height = myImage.height;
+		var width = myImage.width;
+
+		const insertTimeline = event => {
+			console.log(event.clientX);
+		}
+
 		return(
-			<svg className = "mainImage" viewBox = "0 0 1000 1000">
+			<svg className = "mainImage" viewBox = {"0 0 " + width + " " + height} height = {height} width = {width} onDoubleClick = {insertTimeline}>
 				<image className = "actualImage" xlinkHref = {props.imags[props.chosen]}/>
+				<line x1 = "0" x2 = {width} y1 = {5*height/8} y2 = {5*height/8} stroke = "red" strokeWidth = "2"/>
 			</svg>
 		)
 	}
